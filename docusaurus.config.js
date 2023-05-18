86856eb2b88ed373e8246b09d9f49bcc9f08bcb3
Rohan Tangri <rohan.tangri@gmail.com>
2023-05-18 20:17:11 +0100

    revert url changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://compasslabs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dojo',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -110,7 +110,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/intro',
+                to: '/docs/intro',
               },
             ],
           },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/dojo/docs/intro">
+            to="/docs/docs/intro">
             Test your own strategy within 5mins ⏱️
           </Link>
         </div>
