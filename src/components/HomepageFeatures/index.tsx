a26d08f9f62cba9e7b01f2b75cf083e642c68da7
Lukas Scheucher <scheuclu@gmail.com>
2023-05-22 14:13:17 +0300

    Updated docs.

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+// import Image from 'next/image';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>> ;
   description: JSX.Element;
 };
+
+type PNGFeatureItem = {
+  title: string;
+  dest: string;
+  description: JSX.Element;
+};
+
 
 const FeatureList: FeatureItem[] = [
   {
@@ -47,11 +55,76 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
+
+const PNGFeatureList: PNGFeatureItem[] = [
+  {
+    title: 'Data Sourcing',
+    dest: require('@site/static/img/data.png').default,
+    description: (
+      <>
+        Utilize dojo’s on- and off-chain data capabilities for data ingestion and strategy evaluation.
+      </>
+    ),
+  },
+  {
+    title: 'Simulation',
+    dest: require('@site/static/img/simulation.png').default,
+    description: (
+      <>
+        Test and train strategies through dojo’s agent-based simulator to simulate transactions at the EVM smart contract level.
+      </>
+    ),
+  },
+  {
+    title: 'Intelligence',
+    dest: require('@site/static/img/intelligence.png').default,
+    description: (
+      <>
+        Meet strategy goals through parameter optimization at every time step.
+      </>
+    ),
+  },
+  {
+    title: 'Analysis',
+    dest: require('@site/static/img/analysis.png').default,
+    description: (
+      <>
+        Gain insights into strategy performance through tailored agent metric tracking implementation.
+      </>
+    ),
+  },
+];
+
+
+
+
 function Feature({title, Svg, description}: FeatureItem) {
+  console.log(Svg);
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--2')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
+        {/* <Image
+          src="/profile.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        /> */}
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+
+function PNGFeature({title, dest, description}: PNGFeatureItem) {
+  return (
+    <div className={clsx('col')}>
+      <div className="text--center">
+        <img src= {dest} alt="Logo" style={{ height: 200 }} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
@@ -72,8 +145,13 @@ export default function HomepageFeatures(): JSX.Element {
           Designed to provide the infrastructure to scale research and productionisation DeFi strategies 
         </div> */}
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+          {PNGFeatureList.slice(0,2).map((props, idx) => (
+            <PNGFeature key={idx} {...props} />
+          ))}
+        </div>
+        <div className="row">
+          {PNGFeatureList.slice(2,4).map((props, idx) => (
+            <PNGFeature key={idx} {...props} />
           ))}
         </div>
       </div>
