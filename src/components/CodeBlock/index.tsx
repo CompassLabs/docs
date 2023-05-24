b5cb2366ba35feb330265cbfaf310cdd4c7d3db6
Lukas Scheucher <scheuclu@gmail.com>
2023-05-24 17:14:58 +0300

    Implemented Codeblock feature.

---
+++ src/components/CodeBlock/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+// import Image from 'next/image';
+
+
+
+
+export default function CodeBlock(props: any): JSX.Element {
+  console.log("test");
+  return (
+    <section className={"body"}>
+      <div className="container">
+        {props.name}
+        asdasdasdasdasdasda
+
+
+      </div>
+    </section>
+  );
+}
