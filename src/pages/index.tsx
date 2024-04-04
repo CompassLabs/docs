877661ad2f862a75f675fa5da3601d5b80102557
Lukas Scheucher <scheuclu@gmail.com>
2024-04-04 17:11:45 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <script defer data-domain="docs.compasslabs.ai" src="https://plausible.io/js/script.js"></script>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
