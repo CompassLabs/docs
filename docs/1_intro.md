3030095368a1a1c98c2ad29cf347fba8b9d13d3e
Stefan Page <90534062+stefpage@users.noreply.github.com>
2024-04-16 17:44:57 -0300

    updated home page pointer

--- docs/1_intro.md
+++ docs/1_intro.md
@@ -1,4 +1,5 @@
 ---
+slug: /
 sidebar_position: 1
 ---
 
@@ -12,55 +13,72 @@ sidebar_position: 1
 
 :::
 
-
 [Compass Labs](https://compasslabs.ai/) 🧭 developed `dojo`, an agent-based DeFi simulation software operating at the EVM smart contract for realistic and risk-free testing and training of DeFi strategies and smart contracts.
 
-*Dojo is derived from the Japanese words representing immersive learning, and we believe that our solution captures the essence of this concept* ✨
-
+_Dojo is derived from the Japanese words representing immersive learning, and we believe that our solution captures the essence of this concept_ ✨
 
 ## <icon icon="square-check" size="m" color="#1e1c2d"/> Features
+
 ### <icon icon="square-check" size="m" color="#1e1c2d"/> Runs at smart-contract level
-   - `dojo` forks the actual blockchain and runs on the same smart contracts that would be run in real life.
-   - This eliminates concerns about bugs in the model or missed assumptions about the protocol, and enables capturing micro-effects that impact simulation results, such as precision and rounding.
- 
+
+- `dojo` forks the actual blockchain and runs on the same smart contracts that would be run in real life.
+- This eliminates concerns about bugs in the model or missed assumptions about the protocol, and enables capturing micro-effects that impact simulation results, such as precision and rounding.
+
 ### <icon icon="square-check" size="m" color="#1e1c2d"/> Runs locally on your machine
-   - `dojo` operates fully on your local machine.
-   - There is no sharing of information about your strategies or simulations with us or any other party.
- 
+
+- `dojo` operates fully on your local machine.
+- There is no sharing of information about your strategies or simulations with us or any other party.
+
 ### <icon icon="square-check" size="m" color="#1e1c2d"/> Simple Python interface
-   - `dojo's` interface allows full experimentation with strategies using Python.
-   - Take advantage of Python's rich ecosystem of tools for data analysis and visualisation.
- 
+
+- `dojo's` interface allows full experimentation with strategies using Python.
+- Take advantage of Python's rich ecosystem of tools for data analysis and visualisation.
+
 ### <icon icon="square-check" size="m" color="#1e1c2d"/> Transaction ordering
-   - Directly edit the order of transaction execution for simulating various MEV scenarios.
+
+- Directly edit the order of transaction execution for simulating various MEV scenarios.
 
 ### <icon icon="square-check" size="m" color="#1e1c2d"/> Custom data integration
-   - `dojo` sources it's own protocol events data for a subset of environments.
-   - For environments we don't natively support, you can load in your existing datasets.
 
-___
+- `dojo` sources it's own protocol events data for a subset of environments.
+- For environments we don't natively support, you can load in your existing datasets.
+
+---
+
 ## 👀 Upcoming
-### 👀 Market impact model
-Simulate the market's response to your agent actions. With support from Senior Fellows at the Alan Turing Institute, Imperial College London, and the Oxford MAN Institute, dojo will include more complex market scenario models.
-### 👀 MEV models
-Simulate block inclusion probability and transaction ordering.
-### 👀 CEX data
-Get access to CEX spot and derivatives data for you simulations.
-### 👀 Multi protocol/chain simulations
- Conduct simulations across multiple protocols and chains simultaneously.
-### 👀 Execution Environment
-Transfer your strategy from the Dojo fork to mainnet without any modifications, since `dojo` runs at the smart contract level. 
 
-___
-##  ⬇️ Protocol integrations
+### 👀 Market impact model
+
+Simulate the market's response to your agent actions. With support from Senior Fellows at the Alan Turing Institute, Imperial College London, and the Oxford MAN Institute, dojo will include more complex market scenario models.
+
+### 👀 MEV models
+
+Simulate block inclusion probability and transaction ordering.
+
+### 👀 CEX data
+
+Get access to CEX spot and derivatives data for you simulations.
+
+### 👀 Multi protocol/chain simulations
+
+Conduct simulations across multiple protocols and chains simultaneously.
+
+### 👀 Execution Environment
+
+Transfer your strategy from the Dojo fork to mainnet without any modifications, since `dojo` runs at the smart contract level.
+
+---
+
+## ⬇️ Protocol integrations
+
 `dojo` currently integrates with **UniswapV3** and **AAVEv3** on **Ethereum**.
 
-If you want your chain or protocol to be integrated with `dojo`, [reach out](mailto:elisabeth@compasslabs.ai)! 
-
+If you want your chain or protocol to be integrated with `dojo`, [reach out](mailto:elisabeth@compasslabs.ai)!
 
 ## 🥳 Get access to `dojo`
+
 `dojo` is currently in beta testing. Reach out for priority access & feel free to contact us with any questions or feedback:
+
 - [Support via email](mailto:elisabeth@compasslabs.ai)
 - [@labs_compass](https://twitter.com/labs_compass) on Twitter
 - [compasslabs.ai](https://compasslabs.ai/) for general information on Compass Labs
-
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,51 +1,49 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 // const katex = (await import('rehype-katex')).default;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'dojo',
-  tagline: 'An end-to-end DeFi research platform.', //an end-to-end DeFi research platform
+  title: "dojo",
+  tagline: "An end-to-end DeFi research platform.", //an end-to-end DeFi research platform
   customFields: {
     // Put your custom environment here
     branch: process.env.BRANCH,
   },
-  favicon: 'img/compass_logo.svg',
+  favicon: "img/compass_logo.svg",
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 
   // Set the production url of your site here
-  url: 'https://compasslabs.github.io',
+  url: "https://compasslabs.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'compasslabs', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "compasslabs", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   plugins: [
@@ -59,16 +57,15 @@ const config = {
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: 'tutorial',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/CompassLabs/docs/tree/main/docs',
+          editUrl: "https://github.com/CompassLabs/docs/tree/main/docs",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
@@ -80,10 +77,10 @@ const config = {
         //     'https://github.com/CompassLabs/docs/tree/main/blog',
         // },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-EQSZ3TK15C',
+          trackingID: "G-EQSZ3TK15C",
           anonymizeIP: true,
         },
       }),
@@ -92,11 +89,11 @@ const config = {
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
@@ -104,53 +101,54 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/compass_logo.svg',
+      image: "img/compass_logo.svg",
       colorMode: {
         disableSwitch: true,
       },
       navbar: {
-        title: 'dojo',
+        title: "Dojo Docs",
+
         logo: {
-          alt: 'CompassLabs Logo',
-          src: '/img/compass_logo.png',
+          alt: "CompassLabs Logo",
+          src: "/img/compass_logo.png",
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
+          // {
+          //   type: "docSidebar",
+          //   sidebarId: "tutorialSidebar",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/CompassLabs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/CompassLabs",
+            label: "GitHub",
+            position: "right",
           },
           {
-            href: 'https://readthedocs.compasslabs.ai',
-            label: 'Code reference',
-          }
+            href: "https://readthedocs.compasslabs.ai",
+            label: "Code reference",
+          },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/tutorial/intro',
+                label: "Tutorial",
+                to: "/tutorial/intro",
               },
               {
-                label: 'Code reference',
-                href: 'https://readthedocs.compasslabs.ai',
+                label: "Code reference",
+                href: "https://readthedocs.compasslabs.ai",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               // {
               //   label: 'Stack Overflow',
@@ -161,25 +159,25 @@ const config = {
               //   href: 'https://discord.gg/fyrXhZtmc5',
               // },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/labs_compass',
+                label: "Twitter",
+                href: "https://twitter.com/labs_compass",
               },
               {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/company/compass-labs-ai/',
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/company/compass-labs-ai/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               // {
               //   label: 'Blog',
               //   to: '/blog',
               // },
               {
-                label: 'GitHub',
-                href: 'https://github.com/CompassLabs',
+                label: "GitHub",
+                href: "https://github.com/CompassLabs",
               },
             ],
           },
@@ -194,4 +192,3 @@ const config = {
 };
 
 module.exports = config;
-
